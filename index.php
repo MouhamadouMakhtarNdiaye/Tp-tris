84c62a1b62bb2e07c27ee9fa7432c3b77b05ef30
Makhtar <m0uhamadoundiay3@gmail.com>
2026-05-21 12:07:50 +0100

    La fonction de Trie à Bulle

--- index.php
+++ index.php
@@ -3,13 +3,19 @@ require_once "tris.php";
 $tab = [5, 3, 8, 1, 4];
 $n = count($tab);
 
-$trie = triBulles ( $tab );
-echo implode (', ', $trie ). "\n";
+$trieB = triBulles ( $tab );
+echo implode (', ', $trieB ). "\n";
 
-$tailles = [10, 100 , 500 , 1000 , 2000 , 5000 , 10000];
+$tailles = [100 , 500 , 1000 , 2000 , 5000 , 10000];
+
  foreach ( $tailles as $n) {
     $tab = range ($n , 1); // [n, n -1, ... , 2, 1] = cas defavorable
     $time = triBullesChrono ( $tab );
     $nbComp = triBullesCompte ( $tab );
     echo "n = $n : $nbComp comparaisons , $time ms\n";
  }
+
+
+$tab = [5, 3, 8, 1, 4];
+$trieS = triSelection ( $tab );
+echo implode (', ', $trieS );
--- tris.php
+++ tris.php
@@ -18,17 +18,16 @@ function triBullesCompte($tab){
     $n = count($tab);
     $compt = 0;
     for ($i =0 ; $i< $n-1; $i++){
-        for ($j = 0 ; $j < $n-$i-1; $j++ ){
+        for ($j = 0 ; $j < $n-1-$i; $j++ ){
             $compt= $compt+1;
             if ($tab[$j]>$tab[$j +1]){
                 $tmp = $tab[$j];
-            $tab[$j] = $tab[$j+1];
-            $tab[$j+1] = $tmp;
-            $compt++;
+                $tab[$j] = $tab[$j+1];
+                $tab[$j+1] = $tmp;
             }
         }
-    return $compt;
     }
+    return $compt;
 }
 
 
@@ -38,3 +37,25 @@ function triBullesChrono ( $tab ) {
     $tf = microtime(true);
     return round(($tf - $td)*1000 , 2);
 }
+
+function triSelection ( $tab ) {
+    $n = count ( $tab );
+    for ($i = 0; $i < $n - 1; $i ++) {
+    $indiceMin = $i;
+        for ($j = $i + 1; $j < $n; $j ++) {
+            if ( $tab [$j] < $tab [ $indiceMin ]) {
+                $indiceMin = $j;
+            }
+        }
+        if ( $indiceMin !== $i) {
+            $temp = $tab [$i ];
+            $tab [$i] = $tab [ $indiceMin ];
+            $tab [ $indiceMin ] = $temp ;
+        }
+    }
+    return $tab;
+}
+
+
+
+
